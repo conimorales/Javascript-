b217175f9dfbbd312280e369e1ec8ec03068f258
Constanza Morales A <61435709+conimorales@users.noreply.github.com>
2022-12-13 23:50:24 -0300

    fetch
    fetch

--- proyecto_final/js/fetch.js
+++ proyecto_final/js/fetch.js
@@ -3,20 +3,20 @@ const tabla = document.querySelector('#tests-table tbody');
 let json = {"name":"Gasto 1", "id":"1", "description":"Ejemplo 1", "cost":0}    
 
 
+let file = "\js/data.json"
+fetch (file)
+    .then( response => response.json() )
+    .then( data => mostrarData(data) )
+    .catch( error => console.log(error) )
 
+const mostrarData = (data) => {
+    console.log(data)
+    let body = ""
+    for (let i = 0; i < data.length; i++) {      
+        body+=`<tr><td>${data[i].name}</td><td>${data[i].description}</td><td>${data[i].cost}</td>
+        </tr>`
+        
+    }
+    document.getElementById('tests-table').innerHTML = body
 
-        fetch("data.json")
-            .then( response => response.json() )
-            .then( data => mostrarData(data) )
-            .catch( error => console.log(error) )
-
-        const mostrarData = (data) => {
-            console.log(data)
-            let body = ""
-            for (var i = 0; i < data.length; i++) {      
-                body+=`<tr><td>${data[i].id}</td><td>${data[i].name}</td></tr>`
-            }
-            document.getElementById('tests-table').innerHTML = body
-
-        }
-
+}
